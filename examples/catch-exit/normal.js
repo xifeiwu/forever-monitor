200e4d328a3e87b3ef859ff1b72e1da0d741ea33
wuxifei <wuxifei@finupgroup.com>
2019-05-27 11:04:15 +0800

    remove unused examples

--- examples/catch-exit/normal.js
+++
@@ -1,3 +0,0 @@
-setTimeout(() => {
-	console.log('exit after 3000ms');
-}, 3000);
--- examples/env-passed/env-vars.js
+++
@@ -1,8 +0,0 @@
-console.log(JSON.stringify({
-  foo: process.env.FOO,
-  bar: process.env.BAR
-}));
-process.send(JSON.stringify({
-  foo: process.env.FOO,
-  bar: process.env.BAR
-}));
