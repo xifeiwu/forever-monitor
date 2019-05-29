fcdff214502f99417e92e38eab0c39452e8b5605
wuxifei <wuxifei@finupgroup.com>
2019-05-29 10:55:07 +0800

    update demo of with-debug

--- examples/with-debug/child.js
+++ examples/with-debug/child.js
@@ -1,5 +1,23 @@
+const path = require('path');
+const busybox = require('busybox');
+const debug = busybox.debug;
+
+const dm = debug(process.pid);
+
+debug.getState().setConfigs({
+  debug: '*',
+  useColors: true,        // false
+  toFile: path.resolve(__dirname, `${process.pid}`)
+});
+
 var count = 0;
 setInterval(() => {
   count++;
-  console.log(`count: ${count}`);
+  if (count % 2 === 0) {
+    console.log(`count: ${count}`);
+  } else {
+    dm(`count: ${count}`);
+  }
 }, 1000);
+
+
--- examples/with-debug/main.js
+++ examples/with-debug/main.js
@@ -2,31 +2,52 @@ var util = require('util'),
     path = require('path'),
     Monitor = require('../../'),
     script = path.join(__dirname, 'child.js');
+
+const busybox = require('busybox');
+const debug = busybox.debug;
+
+const dm = debug('monitor');
+const dm1 = debug('monitor1');
+const dm2 = debug('monitor2');
+
+debug.getState().setConfigs({
+  debug: '*',
+  useColors: false,        // false
+  toFile: path.resolve(__dirname, 'monitor.out')
+});
         
 var monitor1 = new Monitor(script, {
-  silent: false,
+  silent: true,
   env: {
     PORT: 8000
   }
 });
 monitor1.on('stdout', buf => {
-  console.log(buf);
+  console.log(buf.toString());
+  dm1(buf.toString());
 });
 monitor1.on('stderr', buf => {
-  console.log(buf);
+  console.log(buf.toString());
+  dm1(buf.toString());
 });
 monitor1.start();
 
 var monitor2 = new Monitor(script, {
-  silent: false,
+  silent: true,
   env: {
     PORT: 8000
   },
 });
 monitor2.on('stdout', buf => {
-  console.log(buf);
+  console.log(buf.toString());
+  dm2(buf.toString());
 });
 monitor2.on('stderr', buf => {
-  console.log(buf);
+  console.log(buf.toString());
+  dm2(buf.toString());
 });
 monitor2.start();
+
+setInterval(() => {
+  dm(new Error('error of monitor'));
+}, 10 * 1000);
